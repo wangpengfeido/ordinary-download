ddd68b495ac80373fc0a939160b5bb4989b7e7f1
wangpengfeido <wangpengfeido@126.com>
2022-07-24 11:41:37 +0800

    add mui

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,13 @@
 import React, { FC } from 'react';
+import Button from '@mui/material/Button';
 
 const App: FC = () => {
-  return <div>this is app.</div>;
+  return (
+    <div>
+      this is app.
+      <Button>Hello</Button>
+    </div>
+  );
 };
 
 export default App;
