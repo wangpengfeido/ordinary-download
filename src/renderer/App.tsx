542188331039cbf3f954b3fd23c190106bf8acf4
wangpengfeido <wangpengfeido@126.com>
2022-07-25 01:03:27 +0800

    ui

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,11 +1,23 @@
 import React, { FC } from 'react';
-import Button from '@mui/material/Button';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 
 const App: FC = () => {
   return (
-    <div>
-      this is app.
-      <Button>Hello</Button>
+    <div style={{ width: '100%', height: '100%', display: 'flex' }}>
+      <Drawer variant="permanent" open={true}>
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <FileDownloadIcon></FileDownloadIcon>
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Drawer>
+      dsf
     </div>
   );
 };
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,3 +1,6 @@
+import './style/vars.scss';
+import './style/app.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
